044cb138b8db01b33d16d88171d4a4a73dc45be5
DataKumpel <tmbenkenstein@gmail.com>
2025-11-07 08:54:23 +0100

    Added ratatui-explorer and created basic app struct

--- src/app.rs
+++ src/app.rs
@@ -3,3 +3,23 @@
 //! Contains the App-structure, that watches over the whole state of the application. This structure
 //! orchestrates all the other modules and contains references for buffers, editor state
 //! and UI states.
+
+use ratatui_explorer::FileExplorer;
+
+pub struct App {
+    pub should_quit: bool,
+    pub file_explorer: FileExplorer,
+}
+
+impl App {
+    pub fn new() -> Result<Self, std::io::Error> {
+        Ok(Self {
+            should_quit: false,
+            file_explorer: FileExplorer::new()?,
+        })
+    }
+
+    pub fn quit(&mut self) {
+        self.should_quit = true;
+    }
+}
